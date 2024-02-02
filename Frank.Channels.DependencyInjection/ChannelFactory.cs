d201fdbb1f96f80220b6ccb818b4eb0d05a41924
Frank R. Haugen <frank.haugen@gmail.com>
2024-02-02 11:07:49 +0100

    Update ChannelFactory.cs

--- Frank.Channels.DependencyInjection/ChannelFactory.cs
+++ Frank.Channels.DependencyInjection/ChannelFactory.cs
@@ -7,7 +7,7 @@ public class ChannelFactory : IChannelFactory
 {
     private readonly ConcurrentDictionary<string, object> _cache = new();
     
-    public Channel<T> CreateChannel<T>() where T : class => _cache.GetOrAdd(nameof(T), Value<T>) as Channel<T> ?? throw new InvalidOperationException($"{nameof(Channel<T>)} not found");
+    public Channel<T> CreateChannel<T>() where T : class => _cache.GetOrAdd(typeof(T).Name, Value<T>) as Channel<T> ?? throw new InvalidOperationException($"Channel<{typeof(T).Name}> not found");
 
     private static Channel<T> Value<T>(string arg) where T : class =>
         Channel.CreateUnbounded<T>(new UnboundedChannelOptions()
